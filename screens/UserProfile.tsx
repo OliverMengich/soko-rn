76c1099dcc0443989260900a2f5be997350b9bcc
OliverMengich <kemeioliver2000@gmail.com>
2023-08-06 15:38:29 +0300

    User Profile page

---
+++ screens/UserProfile.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import {View, Text } from 'react-native';
+
+function UserProfile() {
+    return (
+        <View>
+            <Text>User Profile</Text>
+        </View>
+    );
+}
+export default UserProfile;
