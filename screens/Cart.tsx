405852cd4cfd9d939915313f0780fcfae90d319b
OliverMengich <kemeioliver2000@gmail.com>
2023-08-08 21:05:35 +0300

    Cart

--- screens/Cart.tsx
+++ screens/Cart.tsx
@@ -1,12 +1,19 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StatusBar, StyleSheet, useColorScheme} from 'react-native';
 function Cart() {
+    const isDarkMode = useColorScheme() === 'dark';
     return (
         <View>
-            <Text>Cart</Text>
+            <StatusBar
+                barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+                backgroundColor={'#ccc'}
+            />
+            <Text style={styles.text}>Cart</Text>
         </View>
     );
 }
-
+const styles = StyleSheet.create({
+    text: {color: '#000'},
+});
 export default Cart;
